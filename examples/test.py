f2057a76b8f9673f17706812a1959366b38df08f
Daiane Iglesia Dolci <daia.dolci@gmail.com>
2023-05-04 14:08:29 +0100

    revolvers foolowing the the hrevolve sequence strategy

--- examples/test.py
+++ examples/test.py
@@ -1,17 +1,9 @@
 from checkpoint_schedules.hrevolve_sequence import disk_revolve, revolve_1d, periodic_disk_revolve, hrevolve
 
 
-def compare_sequences(sequence1, sequence2):
-    for i, (char1, char2) in enumerate(zip(sequence1, sequence2)):
-        if char1 != char2:
-            raise ValueError(
-                f"Sequences differ at pos {i}: {char1} is not {char2}"
-            )
-
-
 def test_hrevolve_l2():
     # Check that we reproduce Listing 2 from Herrmann and Pallez (2020).
-    sequence = hrevolve(20, [1, 5, 5], [0, 2, 3], [0, 2, 3])
+    sequence = hrevolve(20, [1, 2, 10], [0, 2, 3], [0, 2, 3])
     print(sequence)
     # assert sequence.storage[0] == [17, 14, 12, 9, 7, 3, 0]
     # assert sequence.storage[1] == [7, 12]
@@ -53,7 +45,7 @@ def test_disk_revolve_l4():
     # compare_sequences(
     #     str(sequence), '[WD_0, F_0->15, WD_16, F_16->31, WD_32,'
     #     ' F_32->47, WD_48, F_48->63, WD_64, F_64->79, WD_80, F_80->90,'
-    #     ' Revolve(9, 2), RD_80, 1D-Revolve(10, 2), RD_64, 1D-Revolve(15, 2),'
+    #     ' Revolve(9, 2), RD_80, revolve_1d1D-Revolve(10, 2), RD_64, 1D-Revolve(15, 2),'
     #     ' RD_48, 1D-Revolve(15, 2), RD_32, 1D-Revolve(15, 2), RD_16,'
     #     ' 1D-Revolve(15, 2), RD_0, 1D-Revolve(15, 2)]'
     # )
@@ -70,7 +62,7 @@ def test_revolve_1d_l4():
 
 def test_disk_revolve_l5():
     # Check that we reproduce Listing 5 from Herrmann and Pallez (2020).
-    sequence = disk_revolve(l=100, cm=2, wd=10, rd=2, ub=0, concat=3)
+    sequence = disk_revolve(l=100, cm=2, wd=5, rd=2, ub=0, concat=3)
     print(sequence)
     # compare_sequences(
     #     str(sequence), "(16, 16, 16, 16, 16, 11; 10)"
@@ -80,6 +72,7 @@ def test_disk_revolve_l5():
 def test_periodic_disk_revolve_l6():
     # Check that we reproduce Listing 6 from Herrmann and Pallez (2020).
     sequence = periodic_disk_revolve(l=10, cm=2, wd=2, rd=1, ub=0)
+    print(sequence)
     # compare_sequences(
     #     str(sequence), '[WD_0, F_0->2, WD_3, F_3->5, WD_6, F_6->8, WM_9,'
     #     ' F_9, B_10, RM_9, B_9, DM_9, RD_6, WM_6, F_6, WM_7, F_7, B_8, RM_7,'
@@ -87,12 +80,16 @@ def test_periodic_disk_revolve_l6():
     #     ' B_4, DM_4, RM_3, B_3, DM_3, RD_0, WM_0, F_0, WM_1, F_1, B_2, RM_1,'
     #     ' B_1, DM_1, RM_0, B_0, DM_0]'
     # )
+# test_hrevolve_l2()test_disk_revolve_l3
+# test_disk_revolve_l3()
+# test_disk_revolve_l4()
 
 
 def test_periodic_disk_revolve_l7():
     # Check that we reproduce Listing 7 from Herrmann and Pallez (2020).
     sequence = periodic_disk_revolve(l=10, cm=2, wd=2, rd=1, ub=0,
                                      one_read_disk=True)
+    print(sequence)
     # compare_sequences(
     #     str(sequence), '[WD_0, F_0->2, WD_3, F_3->5, WD_6, F_6->8, WM_9,'
     #     ' F_9, B_10, RM_9, B_9, DM_9, RD_6, WM_6, F_6, WM_7, F_7, B_8, RM_7,'
@@ -104,8 +101,10 @@ def test_periodic_disk_revolve_l7():
     # assert sequence.disk == [0, 3, 6]
     # assert sequence.makespan == 25
 
-
 test_hrevolve_l2()
-# test_disk_revolve_l3()
-# test_disk_revolve_l4()
-# test_disk_revolve_l5()
+test_disk_revolve_l3()
+test_disk_revolve_l4()
+test_revolve_1d_l4()
+test_disk_revolve_l5()
+test_periodic_disk_revolve_l6()
+test_periodic_disk_revolve_l7()
