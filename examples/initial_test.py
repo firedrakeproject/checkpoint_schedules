c0e8b9d5f01e02967b376be765f7df7216360736
Ig-dolci <daia.dolci@gmail.com>
2023-05-23 17:21:20 +0100

    WIP

--- examples/initial_test.py
+++ examples/initial_test.py
@@ -265,9 +265,9 @@ schedule_list = ['hrevolve', 'periodic_disk_revolve', 'disk_revolve', 'periodic_
                  'multistage', 'two_level', 'mixed']
 
 start = tm.time()
-steps = 10
+steps = 5
 schk = 2
-sdisk = 1
+sdisk = 0
 fwd = execute_fwd()
 bwd = execute_bwd()
 manage = Manage(fwd, bwd, steps, save_ram=schk, save_disk=sdisk, schedule='hrevolve')
