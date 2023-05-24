a370df65af5cc212ab910bfed5135d711ab92300
Iglesia Dolci <daia.dolci@gmail.com>
2023-05-24 08:16:38 +0100

    WIP

--- examples/initial_test.py
+++ examples/initial_test.py
@@ -265,28 +265,28 @@ schedule_list = ['hrevolve', 'periodic_disk_revolve', 'disk_revolve', 'periodic_
                  'multistage', 'two_level', 'mixed']
 
 start = tm.time()
-steps = 100
-schk = 10
+steps = 5
+schk = 2
 sdisk = 0
 fwd = execute_fwd()
 bwd = execute_bwd()
 manage = Manage(fwd, bwd, steps, save_ram=schk, save_disk=sdisk, schedule='hrevolve')
 manage.actions()
-steps = 100
-schk = 20
-sdisk = 0
-manage = Manage(fwd, bwd, steps, save_ram=schk, save_disk=sdisk, schedule='hrevolve')
-manage.actions()
-steps = 100
-schk = 30
-sdisk = 0
-manage = Manage(fwd, bwd, steps, save_ram=schk, save_disk=sdisk, schedule='hrevolve')
-manage.actions()
-steps = 100
-schk = 40
-sdisk = 0
-manage = Manage(fwd, bwd, steps, save_ram=schk, save_disk=sdisk, schedule='hrevolve')
-manage.actions()
+# steps = 100
+# schk = 20
+# sdisk = 0
+# manage = Manage(fwd, bwd, steps, save_ram=schk, save_disk=sdisk, schedule='hrevolve')
+# manage.actions()
+# steps = 100
+# schk = 30
+# sdisk = 0
+# manage = Manage(fwd, bwd, steps, save_ram=schk, save_disk=sdisk, schedule='hrevolve')
+# manage.actions()
+# steps = 100
+# schk = 40
+# sdisk = 0
+# manage = Manage(fwd, bwd, steps, save_ram=schk, save_disk=sdisk, schedule='hrevolve')
+# manage.actions()
 # manage = Manage(fwd, bwd, steps, period=2, schedule='periodic_disk')
 # manage.actions()
 # manage = Manage(fwd, bwd, steps, save_ram=3, schedule='periodic_disk_revolve')
