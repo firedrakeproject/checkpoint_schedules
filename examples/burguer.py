c7ba44b4b956b3b136db936a9fb9f6057850a532
Ig-dolci <daia.dolci@gmail.com>
2023-06-19 17:56:25 +0100

    forward 1D burger

--- examples/burguer.py
+++ examples/burguer.py
@@ -3,45 +3,58 @@ import matplotlib.pyplot as plt
 from scipy.sparse import lil_matrix
 from scipy.sparse.linalg import spsolve
 
-# Parameters
+def matrices(nx, dt, dx, nu):
+    b = nu/(dx*dx)
+    A = lil_matrix((nx, nx),)
+    B = lil_matrix((nx, nx))
+    for i in range(1, nx - 1):
+        A[i, i-1] = 1/6 - b*dt/2
+        A[i, i] = 2/3 + b*dt 
+        A[i, i+1] = 1/6 - b*dt/2
+        B[i, i-1] = 1/6 + b*dt/2 
+        B[i, i] = 2/3 - b*dt 
+        B[i, i+1] = 1/6 + b*dt/2
+
+    A[0, 0] = 2/3 + b*dt 
+    A[nx-1, nx-1] = 2/3 + b*dt
+    B[0, 0] = 2/3 - b*dt 
+    B[nx-1, nx-1] = 1/6 + b*dt/2
+    return A, B
+
 L = 1.0  # Length of the domain
-nx = 100  # Number of grid points
+nx = 50  # Number of grid points
 T = 1.0  # Final time
 nt = 100  # Number of time steps
-nu = 0.001  # Viscosity coefficient
+nu = 0.01  # Viscosity coefficient
 degree = 2  # Degree of the Lagrange element
 
-# Discretization
 dx = L / (nx - 1)
-dt = T / nt
+dt = 0.1
 
-# Initialize grid
 x = np.linspace(0, L, nx)
 u = np.sin(np.pi*x)
 
-# Initialize solution
+
 u_new = np.zeros(nx)
-
-# Assemble the stiffness matrix
-K = lil_matrix((nx, nx))
-for i in range(1, nx - 1):
-    K[i, i-1] = -nu / dx**2
-    K[i, i] = 2 * nu / dx**2
-    K[i, i+1] = -nu / dx**2
-
-# Time-stepping
+b = nu/(dx*dx)
 for n in range(nt):
-    # Compute the new solution at each grid point
+    A, B = matrices(nx, dt, dx, nu)
+    d = B.dot(u)
     for i in range(1, nx - 1):
-        u_new[i] = u[i] - dt / (2 * dx) * (u[i+1]**2 - u[i-1]**2) \
-                   - dt * K[i, i] * u[i]
-    
-    # Update the solution
+        A[i, i-1] -= dt/4*u[i-1]/dx
+        A[i, i] += dt/4*(u[i-1]- u[i])/dx
+        A[i, i+1] += dt/4*u[i+1]/dx
+        B[i, i-1] += dt/4*u[i-1]/dx
+        B[i, i] -= dt/4*(u[i-1]- u[i])/dx
+        B[i, i+1] -= dt/4*u[i+1]/dx
+
+    u_new = spsolve(A, d)
     u = u_new.copy()
+print(u_new)
 
 # Plot the final solution
 plt.plot(x, u)
 plt.xlabel('x')
 plt.ylabel('u')
-plt.title('Burger Equation: Lagrange Finite Element')
+# plt.title('')
 plt.show()
