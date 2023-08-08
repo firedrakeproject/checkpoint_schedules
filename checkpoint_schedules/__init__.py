b297c05fbae205efafede95e35157ad235eee05f
Iglesia Dolci <daia.dolci@gmail.com>
2023-08-08 20:48:29 +0100

    fixing the warnings

--- checkpoint_schedules/__init__.py
+++ checkpoint_schedules/__init__.py
@@ -1,7 +1,7 @@
-from .schedule import *  # noqa: F401
-from .hrevolve import * # noqa: F401
-from .multistage import * # noqa: F401
-from .none_revolver import * # noqa: F401
-from .hrevolve_sequences import *  # noqa: F401  
-from .mixed import * # noqa: F401
-from .twolevel_binomial import * # noqa: F401
+from .schedule import *  # noqa: F401, F403
+from .hrevolve import *  # noqa: F401, F403
+from .multistage import *  # noqa: F401, F403
+from .none_revolver import *  # noqa: F401, F403
+from .hrevolve_sequences import *  # noqa: F401, F403
+from .mixed import *  # noqa: F401, F403
+from .twolevel_binomial import *  # noqa: F401, F403
