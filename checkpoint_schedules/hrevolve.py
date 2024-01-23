5e60a0c4251cd81a0f34803b1e62ec3720af6ffd
Iglesia Dolci <daia.dolci@gmail.com>
2024-01-23 12:16:10 +0000

    Add copyright.

--- checkpoint_schedules/hrevolve.py
+++ checkpoint_schedules/hrevolve.py
@@ -1,5 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
+# Copyright (C) 2020 - 2024 The University of Edinburgh and Imperial College London
+# Developed by James R. Maddison (j.r.maddison@ed.ac.uk),
+# Daiane I. Dolci (d.dolci@eimperial.ic.ac.uk) and David A. Ham (david.ham@imperial.ac.uk).
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version. This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
+
 """This module contains the checkpointing schedules for the H-Revolve,
 Disk Revolve, Periodic Disk Revolve and Revolve algorithms.
 """
